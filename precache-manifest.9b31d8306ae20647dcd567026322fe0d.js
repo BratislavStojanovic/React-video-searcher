d03913204780869ce4a43aae4fc26f4bed4324ad
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2020-05-02 18:33:30 +0200

    Updates

---
+++ precache-manifest.9b31d8306ae20647dcd567026322fe0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b076a487013be8af3dda86a31dd53e2b",
+    "url": "/React-video-searcher/index.html"
+  },
+  {
+    "revision": "7ea83aa22bcf6c7f7d41",
+    "url": "/React-video-searcher/static/css/main.e9c82ded.chunk.css"
+  },
+  {
+    "revision": "ecec181eee09f54ee97d",
+    "url": "/React-video-searcher/static/js/2.4da615f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-video-searcher/static/js/2.4da615f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea83aa22bcf6c7f7d41",
+    "url": "/React-video-searcher/static/js/main.a1fdbf67.chunk.js"
+  },
+  {
+    "revision": "76401d579485eacf8968",
+    "url": "/React-video-searcher/static/js/runtime-main.1938c093.js"
+  }
+]);
